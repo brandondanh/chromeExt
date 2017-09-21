38eb44a784f6f85a2f2a50ddf18f89de0f5c71e7
Selina Chang-Yi Zawacki <szmoon@gmail.com>
2017-09-21 14:53:21 -0700

    V2-iframe 1st upload

--- background.js
+++ background.js
@@ -1,50 +1,62 @@
-const query = { active: true, currentWindow: true };
-function getURL() {
 
-  chrome.tabs.query(query, (tabs) => {
-    var url = tabs[0].url;
+
+  const chatDiv = document.createElement('div');
+  chatDiv.setAttribute("id", "myBox");
+  chatDiv.style.width = '10px';
+  chatDiv.style.height = '10px';
+  chatDiv.style.position = 'absolute';
+  chatDiv.innerHTML = "<iframe src='https://www.amazon.com' width='' height='' />";
+  document.body.appendChild( chatDiv);
+  // chatDiv.load("https://www.amazon.com");
+  // $('#myBox').html("<iframe src='https://www.amazon.com' width='' height='' />");
+
+// const query = { active: true, currentWindow: true };
+// function getURL() {
+
+//   chrome.tabs.query(query, (tabs) => {
+//     var url = tabs[0].url;
   
-    if (url.indexOf('www.amazon.com') !== -1) {
-      randomSearch('amazon');
-    }
-    else if (url.indexOf('www.ebay.com') !== -1) {
-      randomSearch('ebay');
-    }
+//     if (url.indexOf('www.amazon.com') !== -1) {
+//       randomSearch('amazon');
+//     }
+//     else if (url.indexOf('www.ebay.com') !== -1) {
+//       randomSearch('ebay');
+//     }
 
-  });
+//   });
 
-}
+// }
 
-window.setInterval(getURL, 5000);
+// window.setInterval(getURL, 5000);
 
-function randomSearch(site) {
+// function randomSearch(site) {
 
-  const URL = 'http://setgetgo.com/randomword/get.php';
-  const xhttpGet = new XMLHttpRequest();
-  xhttpGet.onreadystatechange = function() {
-      if (xhttpGet.readyState == 4 && xhttpGet.status == 200) {
-        // do stuff here
-        let searchUrl;
-        var newWind;
-        let searchTerm = xhttpGet.responseText;
+//   const URL = 'http://setgetgo.com/randomword/get.php';
+//   const xhttpGet = new XMLHttpRequest();
+//   xhttpGet.onreadystatechange = function() {
+//       if (xhttpGet.readyState == 4 && xhttpGet.status == 200) {
+//         // do stuff here
+//         let searchUrl;
         
-        if (site === 'amazon') {
-          searchUrl = ("https://duckduckgo.com/?q=!ducky+site%3Aamazon.com%2Fgp%2Fproduct+").concat(searchTerm);
-          newWind = window.open(searchUrl);
-        }
-        else if (site === 'ebay') {
-          searchUrl = ("https://duckduckgo.com/?q=!ducky+site%3Aebay.com%2Fitm+" + searchTerm + "&ia=web").concat(searchTerm);
-          newWind = window.open(searchUrl);
-        }
+//         let searchTerm = xhttpGet.responseText;
         
-        setTimeout(function() {
-          newWind.close();
-        }, 2500);
-      }
-  }; 
+//         if (site === 'amazon') {
+//           searchUrl = ("https://duckduckgo.com/?q=!ducky+site%3Aamazon.com%2Fgp%2Fproduct+").concat(searchTerm);
+//           // var newWind = window.open(searchUrl);
+//           window.location.replace(URL);
+//         }
+//         else if (site === 'ebay') {
+//           var newWind = window.open("https://duckduckgo.com/?q=!ducky+site%3Aebay.com%2Fitm+" + searchTerm + "&ia=web").concat(searchTerm);
+//         }
+        
+//         setTimeout(function() {
+//           newWind.close();
+//         }, 2500);
+//       }
+//   }; 
 
-  xhttpGet.open('GET', URL, true);
-  xhttpGet.send();
+//   xhttpGet.open('GET', URL, true);
+//   xhttpGet.send();
 
-}
+// }
 
