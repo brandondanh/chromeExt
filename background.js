3d4d7d5bd6ef3ee3efca19985b8ca800227b2f1b
szmoon <szmoon@gmail.com>
2017-09-20 23:43:08 -0700

    working version w/o ajax

--- background.js
+++ background.js
@@ -1,43 +1,54 @@
-chrome.tabs.getCurrent(function(tab){
-    alert(tab.url);
-}
-);
+// $(document).ready(function() {
+//   $.get("http://setgetgo.com/randomword/get.php",{},function(data){
+  // $.ajax({ url: 'http://setgetgo.com/randomword/get.php', success: function(data) { alert(data); } });
+  $.get('http://slack-server.elasticbeanstalk.com/messages')
+  .then(function(data) {
+    // document.getElementById('demo').innerHTML = data;
+    alert("boo");
+  });
+  
 
 
-
-// chrome.tabs.getCurrent((tab) => {
-//     alert(tab.title);
-// });
-// alert(window);
-
-// let query = { 'active': true, 'lastFocusedWindow': true };
 const query = { active: true, currentWindow: true };
-
 function getURL() {
-  // chrome.tabs.query({
-  //     active: true,
-  //     currentWindow: true
-  //   }, function(tabs) {
-  //     var tab = tabs[0];
-  //     var url = tab.url;
-  //     alert(url);
-  //   })
-  // }
 
-//     chrome.tabs.query(query, (tabs) => {
-//     var url = tabs[0].url;
-//     alert(url);
-//     //alert(window.location.href);
-//   });
-  chrome.tabs.getCurrent(function(tab) {alert(tab.url);});
+  chrome.tabs.query(query, (tabs) => {
+    var url = tabs[0].url;
+  
+    if (url.indexOf('www.amazon.com') !== -1) {
+      randomSearch('amazon');
+    }
+    else if (url.indexOf('www.ebay.com') !== -1) {
+      randomSearch('ebay');
+    }
+
+  });
+
 }
-window.setInterval(getURL, 2000);
 
-window.addEventListener('hashchange', () => {
-  const searchPage = window.open('https://www.ebay.com');
-  // searchPage.blur();
-  // window.focus();
-  alert('entered');
+window.setInterval(getURL, 5000);
 
-  // changeBackgroundColor("pink");
-});
+function randomSearch(site) {
+
+  // $.ajax({ url: 'http://setgetgo.com/randomword/get.php', success: function(data) { alert(data); } });
+  let searchUrl;
+  // find random word for searchTerm
+  // $.ajax({ url: 'http://setgetgo.com/randomword/get.php', success: function(data) { 
+    let searchTerm = "tree";
+    
+      if (site === 'amazon') {
+        searchUrl = ("https://duckduckgo.com/?q=!ducky+site%3Aebay.com%2Fitm+er&ia=web").concat(searchTerm);
+        var newWind = window.open(searchUrl);
+      }
+      else if (site === 'ebay') {
+        var newWind = window.open("https://www.ebay.com");
+      }
+      
+      setTimeout(function() {
+        newWind.close();
+      }, 1200);
+
+  // } });
+  
+}
+
